b3b3888ada46f505ee2b1f6d60d8daf083aa5cf0
surabhimaheshwari <surabhi95maheshwari@gmail.com>
2022-02-06 23:21:39 +0530

    mad libs generator is added.

---
+++ README.md
@@ -0,0 +1,5 @@
+A mad libs generator is a fun game that is usually played for fun.
+
+This python project is a good start for the beginner level as it has concepts like strings, variables, and concatenation. Mad Libs Generator teaches to manipulate user-inputted data as the Mad Libs refer to a series of inputs that a user enters. The input from the user could be anything. After all the inputs are entered the application takes all the data and arranges it to build a story template. 
+
+It will be fun to read aloud the stories after filling in the blanks. :D
---
+++ madlibsgenerator.py
@@ -0,0 +1,19 @@
+loop = 1
+while (loop < 2):
+
+    adjactive = input('enter adjective : ')
+    color = input('enter a color name : ')
+    thing = input('enter a thing name :')
+    place = input('enter a place name : ')
+    person= input('enter a person name : ')
+    adjactive1 = input('enter a adjactive : ')
+    insect= input('enter a insect name : ')
+    food = input('enter a food name : ')
+    verb = input('enter a verb name : ')
+
+    print('Last night I dreamed I was a ' +adjactive+ ' butterfly with ' + color+ ' splocthes that looked like '+thing+ ' .I flew to ' + place+ ' with my bestfriend and '+person+ ' who was a '+adjactive1+ ' ' +insect +' .We ate some ' +food+ ' when we got there and then decided to '+verb+ ' and the dream ended when I said-- lets ' +verb+ '.')
+
+    print ("------------------------------------------")
+
+    loop = loop + 1
+
